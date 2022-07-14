4f9516f608aa8deb8148960cce6f9ff99d57a1a3
jamakadev <jamakadev@gmail.com>
2022-07-14 16:17:35 -0400

    completed a few 7&8 kyu

--- 7kyu.py
+++ 7kyu.py
@@ -1,6 +1,20 @@
-
+from math import ceil
 def main() -> None:    
-    pass
+    def solution(digits):
+        i, maxNums = 0, 0
+        while i < len(digits):
+            maxNums = int(digits[i:i+5]) if maxNums < int(digits[i:i+5]) else maxNums
+            i += 1
+        return maxNums;
+
+    def factorial(n):
+        if n < 0: return None
+        elif n == 0: return 1
+        else:
+            res = 1
+            for i in range(1, n+1): res *= i
+            return res
+
     
     
 if __name__ == '__main__':
--- 8kyu.py
+++ 8kyu.py
@@ -1,7 +1,18 @@
 
 def main() -> None:    
-    pass
     
+    def sum_array(arr):
+        return sum(list(sorted(arr)[1:-1])) if arr else 0
+
+    
+    def hero(bullets, dragons):
+        return bullets >= dragons * 2
+
+
+    def count_positives_sum_negatives(arr):
+        return [len(list(filter(lambda x: x > 0, arr)))] + [sum(list(filter(lambda x: x < 0, arr)))] if arr else []
+    
+
     
 if __name__ == '__main__':
     main()
