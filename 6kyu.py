46174fed40b8849c2295760f3e74a66df57cd06f
jamakadev <jamakadev@gmail.com>
2022-07-15 14:15:48 -0400

    completed a few 6 kyu problems

--- 6kyu.py
+++ 6kyu.py
@@ -9,14 +9,35 @@ def main() -> None:
           return chr(ord(char)-1)
       
 
+  def dot(n,m):    
+    # return ("+---" * n + "+\n" + "| o " * n + "|\n") * m + ("+---" * n + "+")
+    res = ''
+    x = 0
+    while x < m:
+      if x == 0:
+        for i in range(3):
+          if i % 2: res += f"\n|{' o |' * n}\n"
+          else: res += f"+{'---+' * n}"
+      else:
+        for i in range(2):
+          if not i % 2: res += f"\n|{' o |' * n}\n"
+          else: res += f"+{'---+' * n}"
+      x += 1
+    return res
+
+
+  def sum_dig_pow(a, b):
+    res = []
+    for i in range(a, b+1):
+        temp = 0
+        for j, char in enumerate(str(i)): temp += int(char)**(j+1)
+        if temp == i: res.append(i)
+    return res
+
       
-
-
-
-      
-
-
-  print(find_missing_letter(["a","b","c","d","f"]))
+  print(sum_dig_pow(1, 100))
+  # print(dot(15,7))
+  # print(find_missing_letter(["a","b","c","d","f"]))
 
 
 
